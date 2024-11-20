78f9a73124503010e640a28ab9e5cd9e3be14f43
Tri Nguyen <115333206+TriNguyen52@users.noreply.github.com>
2024-11-20 23:57:39 +0800

    Update client.js

--- src/client.js
+++ src/client.js
@@ -1,4 +1,4 @@
 import { createClient } from '@supabase/supabase-js'
 const URL = 'https://hqdlzdvtecfsnwjueyzz.supabase.co';
-const API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhxZGx6ZHZ0ZWNmc253anVleXp6Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTczMTY0MjUxOCwiZXhwIjoyMDQ3MjE4NTE4fQ.M50WKqb8Z0Oq1cTvjr9x97BbUSsaDxSAmtadUPVs6Sg'
+const API_KEY = ''
 export const supabase = createClient(URL, API_KEY);
